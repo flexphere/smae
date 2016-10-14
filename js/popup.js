e74c5c31df4b975661169ca3b86342a7bbbfa296
ankh davis <ankh@flexphere.com>
2016-10-14 22:52:03 +0900

    テスト用データの削除 CSVダウンロードにfilesaverを導入

--- js/popup.js
+++ js/popup.js
@@ -5,17 +5,8 @@ var vue;
   vue = new Vue({
     el: '#app',
     data: {
-      status: true,
-      message: '',
-      result: {
-        // header:['col1','col2','col3','col4','col5','col6','col7','col8','col9','col10'],
-        // body:{
-        //   'test':{
-        //     stats:['1','2','3','0','5','0','7','100','9','10'],
-        //     data:[1, 2, 3, 4, 5]
-        //   }
-        // }
-      }
+      message: null,
+      result: null
     },
     methods: {
       analyze: function(event){
@@ -24,48 +15,39 @@ var vue;
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
           chrome.tabs.sendMessage(tabs[0].id, 'onCheck',
           function(res) {
-            this.status = res.success;
             if (res.success) {
               self.result = res.data;
+              self.message = null;
             } else {
+              self.result = null;
               self.message = res.message;
             }
           });
         });
+      },
+      download: function(event){
+        var bom = new Uint8Array([0xEF, 0xBB, 0xBF]);
+        var content = this._generate_csv(this.result);
+        var blob = new Blob([ bom, content ], { "type" : "text/csv" });
+        saveAs(blob, 'result.csv');
+      },
+      _generate_csv: function(data){
+        var csv_rows = [];
+
+        //ヘッダー
+        var header = $.merge([], data.header);
+        header.unshift('件名');
+        csv_rows.push(header);
+
+        //データ
+        $.map(data.body, function(value, key){
+          var stats = $.merge([], value.stats);
+          stats.unshift(key);
+          csv_rows.push(stats);
+        });
+        return csv_rows.join("\n");
       }
     }
   });
 
 }).call(this);
-
-function handleDownload(){
-  var bom = new Uint8Array([0xEF, 0xBB, 0xBF]);
-  var content = generate_csv(vue.result);
-  var blob = new Blob([ bom, content ], { "type" : "text/csv" });
-
-  document.getElementById("download").href = window.URL.createObjectURL(blob);
-  if (window.navigator.msSaveBlob) {
-    window.navigator.msSaveBlob(blob, "result.csv");
-    // msSaveOrOpenBlobの場合はファイルを保存せずに開ける
-    window.navigator.msSaveOrOpenBlob(blob, "result.csv");
-  } else {
-    document.getElementById("download").href = window.URL.createObjectURL(blob);
-  }
-}
-
-function generate_csv(data){
-  var csv_rows = [];
-
-  //ヘッダー
-  var header = $.merge([], data.header);
-  header.unshift('件名');
-  csv_rows.push(header);
-
-  //データ
-  $.map(data.body, function(value, key){
-    var stats = $.merge([], value.stats);
-    stats.unshift(key);
-    csv_rows.push(stats);
-  });
-  return csv_rows.join("\n");
-}
