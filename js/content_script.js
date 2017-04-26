ed6b46c0e6a58dcbd74638ce276e442670b1658f
ankh davis <ankh@flexphere.com>
2017-04-26 10:45:37 +0900

    deleted csv downoads

--- js/content_script.js
+++ js/content_script.js
@@ -1,67 +1,35 @@
 (function() {
-
-  var RESULT = [];
-  var RESULT_TMP = [];
-
-
-  /**
-   * ポップアップからのメッセージを取得
-   */
   chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse){
     parse(location.href);
   });
 
-  /**
-   * 件名別の集計データを返す
-   */
-  function finalize(results){
-    console.log(results);
-    return results;
-  }
-
-  /**
-   * 次のページがあるかどうか
-   */
-  function nextpage(dom){
-    var next = $(".paginate [rel='next']", dom);
-    return next.length ? next.eq(0).attr('href') : false;
-  }
-
-  /**
-   * DOM解析し、件名別の集計データを返す
-   */
-  function parse(page_url){
-    var page = page_url.match(/page=([0-9]+)/)
+  function parse(url){
+    var page = url.match(/page=([0-9]+)/)
     page = (page) ? page[0] : "page=1"
-    chrome.runtime.sendMessage({msg: '処理中：' + page}, function(response){
-      var results = [];
-      var ajax = $.ajax({
-        type: 'get',
-        dataType: 'html',
-        url: page_url
-      });
 
+    chrome.runtime.sendMessage({msg: '処理中：' + page}, function(response){
+      var ajax = $.ajax({type: 'get', dataType: 'html', url: url})
       ajax.done(function(html){
-        mails = scrape(html);
+        mails = scrape(html)
         chrome.runtime.sendMessage({data:mails}, function(response){
-          var next_url = nextpage(html);
+          debugger;
+          var next_url = nextpage(html)
           if (next_url) {
-            parse(next_url);
+            parse(next_url)
           } else {
-            chrome.runtime.sendMessage({msg: '完了', status:'done'});
+            chrome.runtime.sendMessage({msg: '完了', done:true})
           }
-        });
-      });
-    });
+        })
+      })
+    })
   }
 
-  /**
-   * ページを解析
-   */
+  function nextpage(dom){
+    var next = $(".paginate [rel='next']", dom)
+    return next.length ? next.eq(0).attr('href') : false
+  }
 
-   function scrape(dom){
-
-    //メール送信要素確認
+  function scrape(dom){
     if ( ! $(".mail_deliveries_list .item_body", dom).length) {
       chrome.runtime.sendMessage({msg: 'メールが見つかりませんでした'});
       return false;
@@ -92,83 +60,24 @@
         } else {
           data[key] = get_val($(selector, item));
         }
-        $(selector, item).css("border", "solid 1px red")
-      });
+      })
 
       mails.push(data)
     });
     return mails
   }
 
-  // function scrape(dom){
-  //   var headers = [];
-  //   var subjects = {};
-  //
-  //   if ( ! $(".mail_deliveries_list .item_body", dom).length) return false;
-  //
-  //   $(".mail_deliveries_list .item_body", dom).each(function(i){
-  //     //件名
-  //     var subject = $(".item_informations .item_name a", this).text();
-  //     // 一時オブジェクト
-  //     var tmp = [];
-  //
-  //     $(".item_elements li", this).each(function(){
-  //       //項目名の取得
-  //       if (!i) {
-  //         var title_main = get_str($(".element_name", this));
-  //         var title_sub = get_str($(".element_name_sub", this));
-  //         headers.push(title_main, title_sub);
-  //       }
-  //       //値の取得
-  //       var value = $(".element_content a", this).length ?
-  //                   get_val($(".element_content a", this)) :
-  //                   get_val($(".element_content", this));
-  //       var value_sub = get_val($(".element_content_sub", this));
-  //       tmp.push(value, value_sub);
-  //     });
-  //
-  //     //件名ごとに配列を作成
-  //     if (typeof subjects[subject] === 'undefined') {
-  //       subjects[subject] = {data:[],stats:[]};
-  //     }
-  //     subjects[subject].data.push(tmp);
-  //   });
-  //
-  //   $.map(subjects, function(v, k) {
-  //     v.stats = v.data.reduce(function(pre, cur){
-  //       for(i=0;i<pre.length;i++){
-  //         cur[i] += pre[i];
-  //       }
-  //       return cur;
-  //     });
-  //   });
-  //
-  //   return {
-  //     header: headers,
-  //     body: subjects
-  //   };
-  // }
-
-  /**
-   * テキストを取得し、改行・スペースを取り除いて返す
-   */
   function get_str(jqObj){
     var text = get_child_text(jqObj);
     return text.replace(/[\s\n\r]/g, '');
   }
 
-  /**
-   * テキストを取得し、数値として返す
-   */
   function get_val(param){
     var text = (typeof(param) == "object") ? get_child_text(param) : param;
     text = text.replace(/[^0-9.]/g, "");
     return parseFloat(text);
   }
 
-  /**
-   * 直下のテキストノードを取得
-   */
   function get_child_text($target, str) {
   	var nodes = $target
   		.contents()
--- js/popup.js
+++ js/popup.js
@@ -1,77 +1,61 @@
-var mails = []
-var subjects = {}
-var vue = null
+var vue = null,
+    mails = null,
+    subjects = null,
+    messages = null,
+    headers = ['件名', '送信数', '再送信数', '再送信率', 'クリック数', 'クリック率', 'ダウンロード数', 'ダウンロード率', '開封数', '開封率', '拒否数', '拒否率']
 
 vue = new Vue({
   el: '#app',
-  data: {
-    messages: [],
-    subjects: subjects,
-  },
+  data: { loading: false, messages:[], headers:[], subjects:{} },
   methods: {
     analyze: function(event){
-      event.preventDefault();
-      var self = this
+      if (this.loading) return false
+      headers, mails, messages = []
       subjects = {}
-      mails = []
-      self.messages = []
-
+      this.loading = true
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, 'onCheck', function(){});
-      });
-    },
-    download: function(event){
-      var bom = new Uint8Array([0xEF, 0xBB, 0xBF]);
-      var content = this._generate_csv(this.result);
-      var blob = new Blob([ bom, content ], { "type" : "text/csv" });
-      saveAs(blob, 'result.csv');
-    },
-    _generate_csv: function(data){
-      var csv_rows = [];
-
-      //ヘッダー
-      var header = $.merge([], data.header);
-      header.unshift('件名');
-      csv_rows.push(header);
-
-      //データ
-      $.map(data.body, function(value, key){
-        var stats = $.merge([], value.stats);
-        stats.unshift(key);
-        csv_rows.push(stats);
-      });
-      return csv_rows.join("\n");
+        chrome.tabs.sendMessage(tabs[0].id, 'onCheck')
+      })
     }
   }
-});
-
+})
 
 chrome.runtime.onMessage.addListener(function(res, sender, sendResponse){
   if (res.msg) {
-    vue.messages.unshift(res.msg)
+    messages.unshift(res.msg)
+    vue.messages = messages
   }
 
   if (res.data) {
     mails = mails.concat(res.data)
-    console.log(mails)
   }
 
-  if (res.status == 'done') {
-    //全てのページの読み込みが完了
+  if (res.done) {
+    // totals by mail subject
     for ( mail of mails ) {
-      //件名がなければ追加
       if ( ! subjects[mail.subject]) {
         subjects[mail.subject] = mail
       } else {
         for (field of Object.keys(mail)) {
-          if (field == 'subject') continue
-          subjects[mail.subject][field] += mail[field]
+          if (field != 'subject') subjects[mail.subject][field] += mail[field]
         }
       }
     }
+
+    //calculate percentages
+    $.each(subjects, function(subject, data){
+      data["resend_per"]   = (data["resend"] / data["send"] * 100).toFixed(2)
+      data["click_per"]    = (data["click"] / data["send"] * 100).toFixed(2)
+      data["download_per"] = (data["download"] / data["send"] * 100).toFixed(2)
+      data["open"]         = data["send"] - data["unread"]
+      data["open_per"]     = (100 - (data["unread"] / data["send"] * 100)).toFixed(2)
+      data["reject_per"]   = (data["reject"] / data["send"] * 100).toFixed(2)
+    })
+
+    vue.headers = headers
     vue.subjects = subjects
-    console.log(subjects)
+    vue.loading = false
   }
 
   sendResponse("OK")
-});
+})
