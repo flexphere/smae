d8bcc2494d2f66ea368e7b719fea1b96df99e3e3
ankh davis <ankh@flexphere.com>
2017-04-26 13:35:11 +0900

    port reconnection & css fix

--- js/content_script.js
+++ js/content_script.js
@@ -1,26 +1,37 @@
 (function() {
-  chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse){
-    parse(location.href);
-  });
+  var live, port
+
+  chrome.runtime.onConnect.addListener(function(p) {
+    live = true
+    port = p
+
+    port.onMessage.addListener(function(msg) {
+      parse(location.href)
+    })
+
+    port.onDisconnect.addListener(function(){
+      live = false
+    })
+  })
+
+
 
   function parse(url){
+    if ( ! live) return
+    var ajax = $.ajax({type: 'get', dataType: 'html', url: url})
     var page = url.match(/page=([0-9]+)/)
     page = (page) ? page[0] : "page=1"
 
-    chrome.runtime.sendMessage({msg: '処理中：' + page}, function(response){
-      var ajax = $.ajax({type: 'get', dataType: 'html', url: url})
-      ajax.done(function(html){
-        mails = scrape(html)
-        chrome.runtime.sendMessage({data:mails}, function(response){
-          debugger;
-          var next_url = nextpage(html)
-          if (next_url) {
-            parse(next_url)
-          } else {
-            chrome.runtime.sendMessage({msg: '完了', done:true})
-          }
-        })
-      })
+    port.postMessage({msg: '処理中：' + page})
+    ajax.done(function(html){
+      mails = scrape(html)
+      port.postMessage({data:mails})
+      var next_url = nextpage(html)
+      if (next_url) {
+        parse(next_url)
+      } else {
+        port.postMessage({msg: '完了', done:true})
+      }
     })
   }
 
@@ -31,16 +42,16 @@
 
   function scrape(dom){
     if ( ! $(".mail_deliveries_list .item_body", dom).length) {
-      chrome.runtime.sendMessage({msg: 'メールが見つかりませんでした'});
-      return false;
+      port.postMessage({msg: 'メールが見つかりませんでした'})
+      return false
     }
 
     var mails = [];
     $(".mail_deliveries_list .item_body", dom).each(function(item_index){
-      var item = this;
-      var subject = '';
-      var tmp = [];
-      var data = {};
+      var item = this
+      var subject = ''
+      var tmp = []
+      var data = {}
       var selectors = {
         "send": ".item_elements li:eq(0) .element_content a",
         "resend": ".item_elements li:eq(0) .element_content_sub",
@@ -56,9 +67,13 @@
       //各種値の取得
       $.each(selectors, function(key, selector){
         if (key == "unread") {
-          data[key] = get_val($(selector, item).text().split("/")[0]);
+          data[key] = get_val($(selector, item).text().split("/")[0])
+        } else if (key == "click" || key == "download") {
+          data[key] = ($(selector + " a", item).length)
+                    ? get_val($(selector + " a", item))
+                    : get_val($(selector, item))
         } else {
-          data[key] = get_val($(selector, item));
+          data[key] = get_val($(selector, item))
         }
       })
 
@@ -68,26 +83,26 @@
   }
 
   function get_str(jqObj){
-    var text = get_child_text(jqObj);
-    return text.replace(/[\s\n\r]/g, '');
+    var text = get_child_text(jqObj)
+    return text.replace(/[\s\n\r]/g, '')
   }
 
   function get_val(param){
-    var text = (typeof(param) == "object") ? get_child_text(param) : param;
-    text = text.replace(/[^0-9.]/g, "");
-    return parseFloat(text);
+    var text = (typeof(param) == "object") ? get_child_text(param) : param
+    text = text.replace(/[^0-9.]/g, "")
+    return parseFloat(text)
   }
 
   function get_child_text($target, str) {
   	var nodes = $target
   		.contents()
   		.filter(function(){
-  			return this.nodeType === 3 // テキストノードか否か
-  			&& /\S/.test(this.data) // 空白か否か
-  			&& $.inArray($(this).parent(), $target) // 直下か否か
-  			&& (typeof str === 'undefined' || str === this.nodeValue); // 文字列の指定がある場合
+  			return this.nodeType === 3
+  			&& /\S/.test(this.data)
+  			&& $.inArray($(this).parent(), $target)
+  			&& (typeof str === 'undefined' || str === this.nodeValue);
       });
-    return nodes.text();
+    return nodes.text()
   }
 
-}).call(this);
+}).call(this)
--- js/popup.js
+++ js/popup.js
@@ -1,61 +1,69 @@
-var vue = null,
-    mails = null,
+var tab = null,
+    port = null
+
+var mails = [],
     subjects = null,
     messages = null,
     headers = ['件名', '送信数', '再送信数', '再送信率', 'クリック数', 'クリック率', 'ダウンロード数', 'ダウンロード率', '開封数', '開封率', '拒否数', '拒否率']
 
-vue = new Vue({
-  el: '#app',
-  data: { loading: false, messages:[], headers:[], subjects:{} },
-  methods: {
-    analyze: function(event){
-      if (this.loading) return false
-      headers, mails, messages = []
-      subjects = {}
-      this.loading = true
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, 'onCheck')
-      })
-    }
-  }
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  tab = tabs[0]
+  port = chrome.tabs.connect(tab.id, {name: "smc"})
+  initiate()
 })
 
-chrome.runtime.onMessage.addListener(function(res, sender, sendResponse){
-  if (res.msg) {
-    messages.unshift(res.msg)
-    vue.messages = messages
-  }
-
-  if (res.data) {
-    mails = mails.concat(res.data)
-  }
-
-  if (res.done) {
-    // totals by mail subject
-    for ( mail of mails ) {
-      if ( ! subjects[mail.subject]) {
-        subjects[mail.subject] = mail
-      } else {
-        for (field of Object.keys(mail)) {
-          if (field != 'subject') subjects[mail.subject][field] += mail[field]
-        }
+function initiate(){
+  vue = new Vue({
+    el: '#app',
+    data: { loaded:false, loading: false, messages:[], headers:[], subjects:{} },
+    methods: {
+      analyze: function(event){
+        if (this.loading) return false
+        headers, mails, messages = []
+        subjects = {}
+        this.loading = true
+        port.postMessage('Run')
       }
     }
+  })
 
-    //calculate percentages
-    $.each(subjects, function(subject, data){
-      data["resend_per"]   = (data["resend"] / data["send"] * 100).toFixed(2)
-      data["click_per"]    = (data["click"] / data["send"] * 100).toFixed(2)
-      data["download_per"] = (data["download"] / data["send"] * 100).toFixed(2)
-      data["open"]         = data["send"] - data["unread"]
-      data["open_per"]     = (100 - (data["unread"] / data["send"] * 100)).toFixed(2)
-      data["reject_per"]   = (data["reject"] / data["send"] * 100).toFixed(2)
-    })
 
-    vue.headers = headers
-    vue.subjects = subjects
-    vue.loading = false
-  }
+  port.onMessage.addListener(function(res) {
+    if (res.msg) {
+      messages.unshift(res.msg)
+      vue.messages = messages
+    }
 
-  sendResponse("OK")
-})
+    if (res.data) {
+      mails = mails.concat(res.data)
+    }
+
+    if (res.done) {
+      // totals by mail subject
+      for ( mail of mails ) {
+        if ( ! subjects[mail.subject]) {
+          subjects[mail.subject] = mail
+        } else {
+          for (field of Object.keys(mail)) {
+            if (field != 'subject') subjects[mail.subject][field] += mail[field]
+          }
+        }
+      }
+
+      //calculate percentages
+      $.each(subjects, function(subject, data){
+        data["resend_per"]   = (data["resend"] / data["send"] * 100).toFixed(2)
+        data["click_per"]    = (data["click"] / data["send"] * 100).toFixed(2)
+        data["download_per"] = (data["download"] / data["send"] * 100).toFixed(2)
+        data["open"]         = data["send"] - data["unread"]
+        data["open_per"]     = (100 - (data["unread"] / data["send"] * 100)).toFixed(2)
+        data["reject_per"]   = (data["reject"] / data["send"] * 100).toFixed(2)
+      })
+
+      vue.headers = headers
+      vue.subjects = subjects
+      vue.loading = false
+      vue.loaded = true
+    }
+  })
+}
